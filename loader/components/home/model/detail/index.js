13dccda96ba083e2ed2bb3605dbd088b567179b6
toyxyz <toyxyz@naver.com>
2024-09-03 12:58:01 +0900

    Add files via upload

--- loader/components/home/model/detail/index.js
+++ loader/components/home/model/detail/index.js
@@ -1,9 +1,16 @@
 import { getLevelInf } from "../../../../static/js/public.js";
-import { api } from "/scripts/api.js";
+//import { api } from "/scripts/api.js";
 import BasicInf from "./basicInf/index.js";
 import Workflow from "./workflow/index.js";
 import Memo from "./memo/index.js";
 import IconRenderer from "../../../public/iconRenderer.js";
+
+function getApi() {
+  const api = window.comfyAPI.api.api;
+  api.api_base = "";
+  return api;
+}
+
 const ext = {
   is_rendering: false,
   name: "ComfyUI-Studio.model.detail",
@@ -129,7 +136,7 @@ export default {
           body.append("type", this.model.type);
           body.append("mtype", this.model.mtype);
           body.append("name", this.model.name);
-          api.api_base = "";
+          const api = getApi();
           api.fetchApi("/cs/upload_thumbnail", { method: "POST", body });
         } catch (error) {
           alert(error);
